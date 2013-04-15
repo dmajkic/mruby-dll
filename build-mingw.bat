aff99cef2bdd91c70b81fda802322374a5d3c3d8
Dusan D. Majkic <dmajkic@lovor.net>
2013-04-15 13:59:00 +0200

    Forced build of mirb and mruby as shared

--- build-mingw.bat
+++ build-mingw.bat
@@ -5,14 +5,21 @@ ruby create_def.rb
 cd mruby
 ruby minirake
 if errorlevel 1 goto :end
+
 set INSTALL_SHARED_DIR=%cd%\bin\shared
 set HOST_PATH=%cd%\build\host
 set BUILD_SHARED_DIR=%HOST_PATH%\bin\shared
+set GEMDIR=%HOST_PATH%\mrbgems
+
 md %INSTALL_SHARED_DIR% %BUILD_SHARED_DIR% 2> nul
 windres %MRUBY_DLL_DIR%\mruby.rc %BUILD_SHARED_DIR%\mrubyres.o
 cd %BUILD_SHARED_DIR%
 gcc -s -shared -Wl,--out-implib,libmruby.a -o mruby.dll %MRUBY_DLL_DIR%\mruby.def %HOST_PATH%\lib\libmruby.a mrubyres.o
 gcc -s -o mrbc.exe %HOST_PATH%\tools\mrbc\mrbc.o %HOST_PATH%\src\print.o libmruby.a
+
+gcc -s -o mruby.exe %GEMDIR%\mruby-bin-mruby\tools\mruby\mruby.o %HOST_PATH%\src\print.o libmruby.a
+gcc -s -o mirb.exe  %GEMDIR%\mruby-bin-mirb\tools\mirb\mirb.o libmruby.a 
+
 for %%f in (mruby.dll libmruby.a *.exe) do copy %%f %INSTALL_SHARED_DIR% > nul
 :end
 endlocal
